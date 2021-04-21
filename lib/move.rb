f9520b4944ccf37977dff39d020953646c1320a8
Alexander Lee <leeal34uw@gmail.com>
2021-04-21 01:44:22 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player)
+def move(board, index, first_player = "X")
   board[index] = player
-  first_player = "X"
 end
